b967221d2a1b35606b73e50051aa57c4043fc32e
rcorpas90 <rafacorpas90@gmail.com>
2025-04-29 11:37:14 +0100

    probando

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
-const app = express()
-const port = 3000
-
+const app = express();
+const port = 3000;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/aa", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 module.exports = app;
